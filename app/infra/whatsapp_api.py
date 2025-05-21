64341a525474d52c579da51bc19c5b67d0f6c61d
root <root@srv831334.hstgr.cloud>
2025-05-21 16:25:23 -0300

    fix: removendo o mention every one

--- app/infra/whatsapp_api.py
+++ app/infra/whatsapp_api.py
@@ -16,7 +16,7 @@ class WhatsAppAPIClient:
 
         payload = {
             "number": number,
-            "mentionsEveryOne": True,
+            "mentionsEveryOne": False,
             "text": text
         }
 
